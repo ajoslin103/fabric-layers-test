b5d45f972b9c4257d48d988f68380ed2fa0b6e79
Al Joslin <allen.joslin@gmail.com>
2025-05-18 22:20:52 -0400

    wip

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import FabricCanvas from './components/FabricCanvas.vue'
+</script>
+
+<template>
+  <div class="app">
+    <h1>Fabric Layers Test</h1>
+    <FabricCanvas />
+  </div>
+</template>
+
+<style scoped>
+.app {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
+
+h1 {
+  color: #2c3e50;
+  margin-bottom: 2rem;
+}
+</style>
